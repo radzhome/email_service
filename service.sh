1f70bfd75cbc5a73a566e96920aef01ebe807d1e
rad <radzhome@gmail.com>
2024-03-31 17:41:05 -0400

    add access logs

--- service.sh
+++ service.sh
@@ -4,4 +4,4 @@ LOGDIR=$(dirname $LOG)
 test -d $LOGDIR || mkdir -p $LOGDIR
 cd /var/web/email_service/
 source /var/web/email_service/.venv/bin/activate
-exec gunicorn --workers=1 --log-file=$LOG --bind=127.0.0.1:8001 --log-level=info email_service.wsgi:application
+exec gunicorn --workers=1 --log-file=$LOG --access-logfile=$LOG --bind=127.0.0.1:8001 --log-level=info email_service.wsgi:application
